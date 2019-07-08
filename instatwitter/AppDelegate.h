64ba98c5fde430de488184f96cc717aa544ace5f
Franklin Sarpong Boampong <frankboamps@fb.com>
2019-07-08 15:30:30 -0700

    Initial Commit

---
+++ instatwitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instatwitter
+//
+//  Created by frankboamps on 7/8/19.
+//  Copyright © 2019 frankboamps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ instatwitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instatwitter
+//
+//  Created by frankboamps on 7/8/19.
+//  Copyright © 2019 frankboamps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
